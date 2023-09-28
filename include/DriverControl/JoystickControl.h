4d126994efceddf8066c5f7368a2c99ba6fd9d21
Github11200 <78944617+Github11200@users.noreply.github.com>
2023-09-28 10:20:13 -0700

    Changed joystick controls

--- include/DriverControl/JoystickControl.h
+++ include/DriverControl/JoystickControl.h
@@ -3,8 +3,28 @@
 using namespace vex;
 
 // 0.01 x
-void JoystickControl()
+/// @brief Controls the drivetrain based on the joystick inputs
+/// @return 0
+int JoystickControl()
 {
     double power = Controller.Axis3.position();
     double turn = Controller.Axis1.position();
+    float deadZone = 5;
+
+    while (true) {
+        power = Controller.Axis3.position();
+        turn = Controller.Axis1.position();
+
+        if (power > deadZone || power < -deadZone)
+            power > 0 ? power = Math.pow(power, 2) * 0.01 : power = -(Math.pow(power, 2) * 0.01);
+        
+        if (turn > deadZone || turn < -deadZone)
+            turn > 0 ? turn = Math.pow(turn, 2) * 0.01 : turn = -(Math.pow(turn, 2) * 0.01);
+
+        Left.spin(vex::forward, power - turn, vex::percentUnits::pct);
+
+        vex::task::sleep(25);
+    }
+
+    return 0;
 }
