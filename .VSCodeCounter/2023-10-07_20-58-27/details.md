738c36f152bfb99238e4afffc0938e8a5a1e32bd
Github11200 <78944617+Github11200@users.noreply.github.com>
2023-10-09 17:38:00 -0700

    finished full PID controller for moving to point

---
+++ .VSCodeCounter/2023-10-07_20-58-27/details.md
@@ -0,0 +1,31 @@
+# Details
+
+Date : 2023-10-07 20:58:27
+
+Directory c:\\Users\\patel\\Desktop\\Jinay\\Robotics\\Code\\OverUnderCode
+
+Total : 16 files,  505 codes, 222 comments, 168 blanks, all 895 lines
+
+[Summary](results.md) / Details / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
+
+## Files
+| filename | language | code | comment | blank | total |
+| :--- | :--- | ---: | ---: | ---: | ---: |
+| [include/Autonomous/DriverAuto/DriverAutonomous.h](/include/Autonomous/DriverAuto/DriverAutonomous.h) | C++ | 90 | 34 | 24 | 148 |
+| [include/Autonomous/Odometry/Odometry.h](/include/Autonomous/Odometry/Odometry.h) | C++ | 58 | 24 | 20 | 102 |
+| [include/Autonomous/PID/PID.h](/include/Autonomous/PID/PID.h) | C++ | 25 | 10 | 6 | 41 |
+| [include/Autonomous/PurePursuit/Path.csv](/include/Autonomous/PurePursuit/Path.csv) | CSV | 0 | 0 | 1 | 1 |
+| [include/Autonomous/PurePursuit/PurePursuit.h](/include/Autonomous/PurePursuit/PurePursuit.h) | C++ | 9 | 0 | 2 | 11 |
+| [include/DriverControl/Catapult.h](/include/DriverControl/Catapult.h) | C++ | 31 | 6 | 7 | 44 |
+| [include/DriverControl/Intake.h](/include/DriverControl/Intake.h) | C++ | 24 | 4 | 6 | 34 |
+| [include/DriverControl/JoystickControl.h](/include/DriverControl/JoystickControl.h) | C++ | 21 | 8 | 8 | 37 |
+| [include/DriverControl/Wings.h](/include/DriverControl/Wings.h) | C++ | 15 | 3 | 4 | 22 |
+| [include/robot-config.h](/include/robot-config.h) | C++ | 17 | 10 | 8 | 35 |
+| [include/vex.h](/include/vex.h) | C++ | 14 | 0 | 4 | 18 |
+| [makefile](/makefile) | Makefile | 12 | 9 | 10 | 31 |
+| [src/main.cpp](/src/main.cpp) | C++ | 77 | 71 | 32 | 180 |
+| [src/robot-config.cpp](/src/robot-config.cpp) | C++ | 23 | 14 | 10 | 47 |
+| [vex/mkenv.mk](/vex/mkenv.mk) | Makefile | 70 | 22 | 19 | 111 |
+| [vex/mkrules.mk](/vex/mkrules.mk) | Makefile | 19 | 7 | 7 | 33 |
+
+[Summary](results.md) / Details / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
--- include/Autonomous/PID/PID.h
+++ include/Autonomous/PID/PID.h
@@ -5,12 +5,36 @@ using namespace vex;
 
 class PID
 {
+private:
+    /// @brief This function returns 1 or -1 based on whether the input number is positive or negative
+    /// @param num The number who's sign you want to figure out
+    /// @return 1 or -1 based on whether the input number is positive or negative
+    int sgn(double num)
+    {
+        if (num >= 0)
+            return 1;
+        else
+            return -1;
+    }
+
+    /// @brief This method finds the minimum angle that the robot has to turn in order to face the target angle
+    /// @param angle This is the angle inside the triangle
+    /// @param targetAngle This is the angle at which we want the robot to be at
+    /// @return The minimum number of degrees the robot has to turn to reach the target angle
+    long double findMinAngle(double angle, double targetAngle)
+    {
+        double minAngle = angle - targetAngle;
+
+        if (minAngle > 180 || minAngle < -180)
+            minAngle = -1 * this->sgn(minAngle) * (360 - fabs(minAngle));
+    }
+
 public:
     /// @brief This method will move the robot to a position on the field
-    /// @param The x position that you want the robot to move to on the field
-    /// @param The y position that you want the robot to move to on the field
-    /// @param The angle at which you want the robot to be at once it is done moving
-    /// @param Whether the x and y values are based off of the assumption that the robot is always starting at point (0, 0)
+    /// @param targetX The x position that you want the robot to move to on the field
+    /// @param targetY The y position that you want the robot to move to on the field
+    /// @param targetTheta The angle at which you want the robot to be at once it is done moving
+    /// @param isRelativeToZero Whether the x and y values are based off of the assumption that the robot is always starting at point (0, 0)
     void MoveToPoint(double targetX, double targetY, double targetTheta = NULL, bool isRelativeToZero = false)
     {
         // The total power for turning and moving forward
@@ -19,22 +43,84 @@ public:
 
         // The errors both while moving forward and turning
         double error = 10;
-        double turnError;
+        double previousError = 0;
+        double turnError = 0;
+        double previousTurnError = 0;
+
+        double integral = 0;
+        double turnIntegral = 0;
+
+        double derivative = 0;
+        double turnDerivative = 0;
+
+        double angle = 0;
+        double turnAngle = 0;
 
         // Constants
         float kP = 0.01;
+        float kI = 0.01;
+        float kD = 0.01;
+
         float turnkP = 0.01;
+        float turnkI = 0.01;
+        float turnkD = 0.01;
+        int maxErrorForIntegral = 100;
+        int maxTurnErrorForTurnIntegral = 90;
+        int dT = 15;
 
         if (!isRelativeToZero)
         {
             while (error >= 3 || (error < 0 && error >= -3))
             {
-                        }
+                /*=================================================================
+                                    Moving towards target point
+                =================================================================*/
+
+                /*****************************ERROR*****************************/
+                error = sqrt(fabs(pow(targetX - x, 2)) + fabs(pow(targetY - y, 2)));
+
+                /*****************************INTEGRAL*****************************/
+                // If the robot has overshot the target or the error is really big then integral should be 0
+                error < 0 || error >= maxErrorForIntegral ? integral = 0 : integral += error;
+
+                /*****************************DERIVATIVE*****************************/
+                derivative = error - previousError;
+
+                /*=================================================================
+                                    Turning towards target point
+                =================================================================*/
+
+                /*****************************TURN ERROR*****************************/
+                angle = atan2(targetY - y, targetX - x);
+                if (angle < 0)
+                    angle += 360;
+                turnError = this->findMinAngle(angle, targetTheta);
+
+                /*****************************TURN INTEGRAL*****************************/
+                turnError < 0 || turnError >= maxTurnErrorForTurnIntegral ? turnIntegral = 0 : turnIntegral += turnError;
+
+                /*****************************TURN DERIVATIVE*****************************/
+                turnDerivative = turnError - previousTurnError;
+
+                // Calculate the powers for going straight and turning
+                power = (error * kP) + (integral * kI) + (derivative * kD);
+                turnPower = (turnError * turnkP) + (turnIntegral * turnkI) + (turnDerivative * turnkD);
+
+                // Spin the motors based on the values
+                Right.spin(vex::directionType::fwd, power + turnPower, vex::voltageUnits::volt);
+                Left.spin(vex::directionType::fwd, power - turnPower, vex::voltageUnits::volt);
+
+                // Update the previous errors
+                previousError = error;
+                previousTurnError = turnError;
+
+                wait(dT, vex::timeUnits::msec);
+            }
         }
     }
 
     /// @brief This method will turn the robot to a certain angle relative to the field
-    /// @param This is the angle at which you want the robot to turn to
+    /// @param targetTheta This is the angle at which you want the robot to turn to
     void Turn(double targetTheta)
     {
     }
