4d4e78bb082410e3d24f5b2bec5dd0150885ba6b
Github11200 <78944617+Github11200@users.noreply.github.com>
2024-02-02 16:35:39 -0800

    Added changes to the joystick curve

--- src/DriverControl/JoystickControl.cpp
+++ src/DriverControl/JoystickControl.cpp
@@ -73,8 +73,13 @@ int JoystickControl()
         // Calculate the power using the curve (we will leave the turning linear)
         power >
                 0
-            ? power = pow(power, 2) * 0.01
-            : power = -(pow(power, 2) * 0.01);
+            ? power = ((sin((M_PI * (power / 100)) - 1.57) * 0.5) + 0.5) * 100;
+            : power = (-((sin((M_PI * (power / 100)) - 1.57) * 0.5) + 0.5)) * 100;
+
+        turn >
+                0
+            ? power = ((sin((M_PI * (turn / 100)) - 1.57) * 0.5) + 0.5) * 100;
+            : power = (-((sin((M_PI * (turn / 100)) - 1.57) * 0.5) + 0.5)) * 100;
 
         if (flipControls)
         {
